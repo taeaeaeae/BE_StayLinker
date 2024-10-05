6157f4fdf5de3ce8395943ea85f96d1fbd126846
Ppajingae <wkmkyj991112@gmail.com>
2024-10-05 16:22:02 +0900

    ✨ feat : Implement SunnySide BE Structure

---
+++ src/main/kotlin/com/yoong/sunnyside/SunnySideApplication.kt
@@ -0,0 +1,11 @@
+package com.yoong.sunnyside
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SunnySideApplication
+
+fun main(args: Array<String>) {
+    runApplication<SunnySideApplication>(*args)
+}
---
+++ src/test/kotlin/com/yoong/sunnyside/SunnySideApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.yoong.sunnyside
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SunnySideApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
