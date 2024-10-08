0d6c5d90d4aeda40870c17b88442035392c5e321
Ppajingae <wkmkyj991112@gmail.com>
2024-10-09 00:05:13 +0900

    ✨ feat Implement RealEstate DTO and TYPE Enum class

---
+++ src/main/kotlin/com/yoong/sunnyside/domain/real_estate/enum_class/HouseType.kt
@@ -0,0 +1,8 @@
+package com.yoong.sunnyside.domain.real_estate.enum_class
+
+enum class HouseType {
+    HANOK, // 한옥
+    APARTMENT, // 아파트
+    TOWN_HOUSE, // 다세대 주택, 연립 주택
+    NON_HOUSING // 비주택 분류 ( 오피스텔, 고시원 등등)
+}
