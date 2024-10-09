2457468a45770d5a52d20c23bcf5277275ed6158
taeaeaeae <haenimhana@gmail.com>
2024-10-09 14:33:56 +0900

    feat: security config 설정

---
+++ src/main/kotlin/com/yoong/sunnyside/infra/security/config/SecurityConfig.kt
@@ -0,0 +1,73 @@
+package com.yoong.sunnyside.infra.security.config
+
+import com.yoong.sunnyside.infra.security.CustomAccessDeniedHandler
+import com.yoong.sunnyside.infra.security.jwt.CustomAuthenticationEntryPoint
+import com.yoong.sunnyside.infra.security.jwt.JwtAuthenticationFilter
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+class SecurityConfig(
+    private val jwtAuthenticationFilter: JwtAuthenticationFilter,
+    private val authenticationEntryPoint: CustomAuthenticationEntryPoint,
+    private val accessDeniedHandler: CustomAccessDeniedHandler,
+) {
+
+    @Bean
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+
+        return http
+            .httpBasic { it.disable() }
+            .formLogin { it.disable() }
+            .csrf { it.disable() }
+            .cors { it.configurationSource(corsConfigurationSource()) }
+            .authorizeHttpRequests {
+                it.requestMatchers(HttpMethod.GET, "/**")
+                    .permitAll()
+                it.requestMatchers(
+                    "/**"
+                )
+                    .permitAll()
+                    .anyRequest().authenticated()
+            }
+            .sessionManagement {
+                it.sessionCreationPolicy(SessionCreationPolicy.NEVER)
+            }
+            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter::class.java)
+            .exceptionHandling {
+                it.authenticationEntryPoint(authenticationEntryPoint)
+                it.accessDeniedHandler(accessDeniedHandler)
+            }
+            .build()
+    }
+
+
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource {
+        val configuration = CorsConfiguration()
+
+        configuration.setAllowedOriginPatterns(listOf("*"))
+        configuration.allowedMethods = listOf("HEAD", "GET", "POST", "PUT", "DELETE", "OPTIONS")
+        configuration.allowedHeaders = listOf("*")
+        configuration.allowCredentials = true
+        configuration.addExposedHeader("Set-Cookie")
+
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", configuration)
+
+        return source
+    }
+}
---
+++ src/main/kotlin/com/yoong/sunnyside/infra/security/jwt/JwtAuthenticationFilter.kt
@@ -0,0 +1,48 @@
+package com.yoong.sunnyside.infra.security.jwt
+
+import com.yoong.sunnyside.infra.security.MemberPrincipal
+import jakarta.servlet.FilterChain
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.http.HttpHeaders
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource
+import org.springframework.stereotype.Component
+import org.springframework.web.filter.OncePerRequestFilter
+
+@Component
+class JwtAuthenticationFilter(private val jwtHelper: JwtHelper) : OncePerRequestFilter() {
+    override fun doFilterInternal(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain,
+    ) {
+        val token = request.getBearerToken()
+        if (token != null) {
+            jwtHelper.validateTokenAndGetClaims(token).onSuccess { claims ->
+                val id = claims.payload.subject.toLong()
+                val role = MemberRole.valueOf(claims.payload["role"] as String)
+
+                val authentication = JwtAuthenticationToken(
+                    principal = MemberPrincipal(
+                        id = id,
+                        role = role,
+                    ),
+                    details = WebAuthenticationDetailsSource().buildDetails(request)
+                )
+
+                SecurityContextHolder.getContext().authentication = authentication
+            }
+        }
+
+        filterChain.doFilter(request, response)
+    }
+
+    private fun HttpServletRequest.getBearerToken(): String? {
+        val header = this.getHeader(HttpHeaders.AUTHORIZATION) ?: return null
+        val prefix = "Bearer "
+        return if (header.startsWith(prefix, ignoreCase = true))
+            header.substring(prefix.length)
+        else null
+    }
+}
