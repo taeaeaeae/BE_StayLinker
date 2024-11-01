2535a14093845c5c9584614d2fee0e8dccf99e1c
Ppajingae <wkmkyj991112@gmail.com>
2024-11-01 22:44:39 +0900

    ✨ Implement chat API

---
+++ src/main/kotlin/com/yoong/sunnyside/domain/chat/service/ChatService.kt
@@ -0,0 +1,13 @@
+package com.yoong.sunnyside.domain.chat.service
+
+import com.yoong.sunnyside.domain.chat.dto.ChatRequestDto
+import com.yoong.sunnyside.domain.chat.dto.ChatResponseDto
+import org.springframework.stereotype.Service
+
+@Service
+class ChatService {
+
+    fun chat(chatRequestDto: ChatRequestDto): ChatResponseDto {
+        TODO()
+    }
+}
