a5224ee1da28256c2dcd131f449c22e5b293e6d3
Ppajingae <wkmkyj991112@gmail.com>
2024-11-04 13:27:41 +0900

    ✨feat Implement Trade Favorite API structure

--- src/main/kotlin/com/yoong/sunnyside/domain/trade/favorite/controller/TradeFavoriteController.kt
+++ src/main/kotlin/com/yoong/sunnyside/domain/trade/favorite/controller/TradeFavoriteController.kt
@@ -10,27 +10,27 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 
-@Tag(name = "커뮤니티 찜하기 관련 API")
+@Tag(name = "중고 거래 찜하기 관련 API")
 @RestController
 @RequestMapping("/community/reply")
 class TradeFavoriteController(
     private val tradeFavoriteService: TradeFavoriteService
 ){
 
-    @Operation(summary = "커뮤니티 댓글 생성 API", description = "커뮤니티 id 값을 넣어 주시면 됩니다")
+    @Operation(summary = "중고 거래 찜하기 API", description = "중고 거래 id 값을 넣어 주시면 됩니다")
     @PostMapping("/{tradeId}")
     fun createFavorite(
         @PathVariable tradeId: Long,
     ): ResponseEntity<DefaultResponseDto>
             = ResponseEntity.status(HttpStatus.OK).body(tradeFavoriteService.createFavorite(tradeId))
 
-    @Operation(summary = "내 찜한 커뮤니티 게시글 조회 API")
+    @Operation(summary = "내 찜한 중고 거래 게시글 조회 API")
     @GetMapping
     fun getFavorite(
     ): ResponseEntity<List<AllTradeResponseDto>>
             = ResponseEntity.status(HttpStatus.OK).body(tradeFavoriteService.getFavorite())
 
-    @Operation(summary = "커뮤니티 찜하기 삭제 API", description = "커뮤니티 id 값을 넣어 주시면 됩니다")
+    @Operation(summary = "중고 거래 찜하기 삭제 API", description = "중고 거래 id 값을 넣어 주시면 됩니다")
     @DeleteMapping("/{tradeId}")
     fun deleteFavorite(
         @PathVariable("tradeId") tradeId: Long,
