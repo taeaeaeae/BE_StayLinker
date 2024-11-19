d6d0eb677bcb5e1a27a3f5126bcf766003d74f9c
Ppajingae <wkmkyj991112@gmail.com>
2024-11-19 15:18:19 +0900

    ✅ Test CommunityFavoriteTest

--- src/main/kotlin/com/yoong/sunnyside/domain/real_estate/favorite/controller/RealEstateFavoriteController.kt
+++ src/main/kotlin/com/yoong/sunnyside/domain/real_estate/favorite/controller/RealEstateFavoriteController.kt
@@ -3,8 +3,10 @@ package com.yoong.sunnyside.domain.real_estate.favorite.controller
 import com.yoong.sunnyside.common.dto.DefaultResponse
 import com.yoong.sunnyside.domain.favorite.controller.FavoriteController
 import com.yoong.sunnyside.domain.real_estate.favorite.service.RealEstateFavoriteService
+import com.yoong.sunnyside.infra.security.MemberPrincipal
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.*
 
 @RestController
@@ -15,13 +17,15 @@ class RealEstateFavoriteController(
 
     @PostMapping
     override fun createFavorite(
-        @PathVariable("realEstateId") id: Long
+        @PathVariable("realEstateId") id: Long,
+        @AuthenticationPrincipal principal: MemberPrincipal
     ): ResponseEntity<DefaultResponse>
-        = ResponseEntity.status(HttpStatus.OK).body(realEstateFavoriteService.createFavorite(id))
+        = ResponseEntity.status(HttpStatus.OK).body(realEstateFavoriteService.createFavorite(id, principal.id))
 
     @DeleteMapping
     override fun deleteFavorite(
-        @PathVariable("realEstateId") id: Long
+        @PathVariable("realEstateId") id: Long,
+        @AuthenticationPrincipal principal: MemberPrincipal
     ): ResponseEntity<DefaultResponse>
-        = ResponseEntity.status(HttpStatus.OK).body(realEstateFavoriteService.deleteFavorite(id))
+        = ResponseEntity.status(HttpStatus.OK).body(realEstateFavoriteService.deleteFavorite(id, principal.id))
 }
