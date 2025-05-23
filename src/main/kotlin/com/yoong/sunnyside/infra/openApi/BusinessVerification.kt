ce9bff2c5fb5c5ec61fb22dd84991b2731312380
taeaeaeae <haenimhana@gmail.com>
2025-05-23 16:15:43 +0900

    feat: 부동산 검색 api 및 기존 검증코드 상세조회로 변경

--- src/main/kotlin/com/yoong/sunnyside/infra/openApi/BusinessVerification.kt
+++ src/main/kotlin/com/yoong/sunnyside/infra/openApi/BusinessVerification.kt
@@ -2,6 +2,8 @@ package com.yoong.sunnyside.infra.openApi
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.yoong.sunnyside.common.exception.ValidException
+import com.yoong.sunnyside.domain.business.dto.BusinessResponse
+import com.yoong.sunnyside.domain.business.dto.BusinessSearchResultResponse
 import com.yoong.sunnyside.infra.web_client.WebClientConfig
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
@@ -31,6 +33,62 @@ class BusinessVerification(
         if (businessRes.EDOffices == null) throw ValidException("부동산 등록번호를 정확하게 입력했는지 확인해주세요.")
 
         return businessRes.EDOffices.field[0]
+    }
+
+    fun searchNameResult(keyword: String): List<BusinessSearchResultResponse> {
+        val url = UriComponentsBuilder.fromHttpUrl(BASE_URL)
+            .queryParam("key", API_KEY)
+            .queryParam("format", "json")
+            .toUriString()
+        val response = webClient.connect()
+            .get()
+            .uri("${url}&bsnmCmpnm=${keyword}")
+            .retrieve()
+            .bodyToMono(String::class.java)
+            .block()
+
+        val businessRes = objectMapper.readValue(response, OfficeInfo::class.java)
+        if (businessRes.EDOffices == null) throw ValidException("검색된 이름의 중개사무소가 존재하지 않습니다.")
+
+        return businessRes.EDOffices.field.map { it.toResponse() }
+    }
+
+
+    fun searchAgentNameResult(keyword: String): List<BusinessSearchResultResponse> {
+        val url = UriComponentsBuilder.fromHttpUrl(BASE_URL)
+            .queryParam("key", API_KEY)
+            .queryParam("format", "json")
+            .toUriString()
+        val response = webClient.connect()
+            .get()
+            .uri("${url}&bsnmCmpnm=${keyword}")
+            .retrieve()
+            .bodyToMono(String::class.java)
+            .block()
+
+        val businessRes = objectMapper.readValue(response, OfficeInfo::class.java)
+        if (businessRes.EDOffices == null) throw ValidException("검색된 대표명이 존재하지 않습니다.")
+
+        return businessRes.EDOffices.field.map { it.toResponse() }
+    }
+
+
+    fun searchCertificateResult(keyword: String): List<BusinessSearchResultResponse> {
+        val url = UriComponentsBuilder.fromHttpUrl(BASE_URL)
+            .queryParam("key", API_KEY)
+            .queryParam("format", "json")
+            .toUriString()
+        val response = webClient.connect()
+            .get()
+            .uri("${url}&jurirno=${keyword}")
+            .retrieve()
+            .bodyToMono(String::class.java)
+            .block()
+
+        val businessRes = objectMapper.readValue(response, OfficeInfo::class.java)
+        if (businessRes.EDOffices == null) throw ValidException("검색된 중개등록번호가 존재하지 않습니다.")
+
+        return businessRes.EDOffices.field.map { it.toResponse() }
     }
 
 
