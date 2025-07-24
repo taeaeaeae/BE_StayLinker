6baaf80b949d4a178bc110f9e7aa94df2d2ee4d1
LeeTaekyoung <46617216+taeaeaeae@users.noreply.github.com>
2025-07-24 15:39:20 +0900

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -49,8 +49,9 @@ dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     testImplementation("io.mockk:mockk:1.13.9")
-    testImplementation("io.kotest:kotest-runner-junit5:5.7.2")
-    testImplementation("io.kotest:kotest-assertions-core:5.7.2")
+    testImplementation("io.kotest:kotest-runner-junit5:5.8.1")
+    testImplementation("io.kotest:kotest-assertions-core:5.8.1")
+    testImplementation("io.kotest:kotest-property:5.8.1")
 }
 
 kotlin {
