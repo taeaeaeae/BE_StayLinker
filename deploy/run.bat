488e5c71e158e518909594aa1d019433e049b743
LeeTaekyoung <46617216+taeaeaeae@users.noreply.github.com>
2025-07-28 21:19:24 +0900

    Delete deploy directory

--- deploy/run.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d %~dp0
-docker-compose down
-docker-compose up -d --build
-start http://localhost:8080/swagger-ui/index.html
-pause
--- deploy/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")"
-docker-compose down
-docker-compose up -d --build
-open http://localhost:8080/swagger-ui/index.html
-echo "✅ Staylinker 서버가 실행되었습니다!"
